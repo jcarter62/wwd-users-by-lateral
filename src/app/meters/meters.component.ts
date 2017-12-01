c71a220ed9f4d8a603e27879db3426c2391e5bcc
Jim Carter <jcarter62@gmail.com>
2017-11-30 16:40:43 -0800

    Implemented meter list & lateral list

--- src/app/meters/meters.component.ts
+++ src/app/meters/meters.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {IMeterDetail} from '../interface/imeterdetail';
+import {MetersService} from '../services/meters.service';
+import {LocalDataSource} from 'ng2-smart-table';
 
 @Component({
   selector: 'app-meters',
@@ -6,10 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meters.component.css']
 })
 export class MetersComponent implements OnInit {
+  meters = Array<IMeterDetail>();
 
-  constructor() { }
+  tableSettings = {
+        columns: {
+            assetid: {title: 'Meter ID'},
+            geo: {title: 'Geo'},
+            lat: {title: 'Lateral'},
+            area: {title: 'Service Area'}
+        },
+        actions: false,
+        noDataMessage: '... Loading ...'
+    };
 
-  ngOnInit() {
+  source: LocalDataSource;
+
+  constructor(
+      private router: Router,
+      private mtrsvc: MetersService
+  ) {
+    this.source = new LocalDataSource(this.meters);
   }
 
+  ngOnInit() {
+    this.mtrsvc.load((result)=>{ this.loaded(); } );
+  }
+
+  private loaded() {
+    this.meters = this.mtrsvc.meters;
+  }
+
+  userRowSelected(obj) {
+
+  }
+
+  resetButton() {
+    console.log('reset button pressed');
+    this.source.setFilter([]);
+    this.source.refresh();
+  }
 }
+
+// userRowSelected(obj) {
+//     console.log(obj);
+//     let lateral = '';
+//     try {
+//         lateral = obj.selected[0].lateralname;
+//     } catch (e) {
+//         lateral = '';
+//     }
+//     console.log('Selected Lateral:' + lateral);
+//
+//     if ( lateral > '' ) {
+//         this.router.navigate(['/lateral/' + lateral] );
+//     }
+//
+// }
